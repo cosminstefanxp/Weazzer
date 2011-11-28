68768b18e85569b6bc97f317b2e3c75571711f81
Cosmin Stefan <cosminstefanxp@gmail.com>
2011-11-28 14:26:44 +0200

    First project init

---
+++ src/weazzer/gui/WeazzerActivity.java
@@ -0,0 +1,13 @@
+package weazzer.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeazzerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
